e5410586d67a488918184cab4a357810e0c3cb8c
Aaron Meadows <aaron9127@gmail.com>
2017-04-28 13:37:07 -0400

    Merging Troy's and Aaron's Changes

--- model.js
+++ model.js
@@ -45,6 +45,7 @@ module.exports = function(done) {
         csvMemberArr[0] = csvMemberArr[0]
           .replace(/e\.i\..*/i, 'Du Pont')
           .replace(/merck.*/i, 'Merck & Co')
+          .repalce(/the travelers*/i, 'Travelers')
           .replace(/(( compan)|( corp)|( incorp)|(\.)|( communica)|( stores)|( common)|( inc)|( group)|( \& co.)).*/i, '')
           .replace(/international business machines/i, 'IBM');
 
